bf5f1dabebbbab1be48a11c8787bd38585bc94e4
ajakobso <91913356+ajakobso@users.noreply.github.com>
2023-01-15 18:18:55 +0200

    create pray text

--- src/scanandpray/__main__.py
+++ src/scanandpray/__main__.py
@@ -1,9 +1,10 @@
+from kivy.properties import StringProperty
 from kivy.uix.button import Button
 from kivy.garden.xcamera import XCamera
 from kivy.uix.floatlayout import FloatLayout
 from roboflow import Roboflow
 from kivy.base import runTouchApp
-import glob
+import os
 
 MIN_CONFIDENCE = 0.5
 API_KEY = "Oaqu2oIbVpT7bCyf1klh"
@@ -25,6 +26,36 @@ class PredictPrayButton(Button):
         self.size_hint = (0.2, 0.2)
         self.pos_hint = {"x": 0.845, "center_y": 0.65}
         self.background_normal = "pray_icon.png"
+
+
+def pray(predictions):
+    first_pray_dict = {'המוציא': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם הַמּוֹצִיא לֶחֶם מִן הָאָרֶץ",
+                       'שהכל': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם שֶׁהַכֹּל ניהיה בִּדְבָרו",
+                       'מזונות': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם בּוֹרֵא מִינֵי מְזוֹנוֹת",
+                       'הגפן': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם בּוֹרֵא פְּרִי הַגָּפֶן",
+                       'העץ': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם בּוֹרֵא פְּרִי הָעֵץ",
+                       'האדמה': "בָּרוּךְ אַתָּה יהֵוָהֵ אֱלהֵינוּ מֶלֶךְ הָעולָם בּוֹרֵא פְּרִי הָאֲדָמָה"}
+    create_souls_pray = "בָּרוּךְ אַתָּה יי אֱלהֵינוּ מֶלֶךְ הָעוֹלָם בּוֹרֵא נְפָשׁוֹת רַבּוֹת" \
+                        " וְחֶסְרוֹנָן עַל כָּל מַה שֶׁבָּרָאתָ לְהַחֲיוֹת בָּהֶם נֶפֶשׁ כָּל חָי. בָּרוּךְ חֵי הָעוֹלָמִים"
+    last_pray_dict = {'המוציא': "ברכת המזון",
+                      'שהכל': create_souls_pray,
+                      'מזונות': "מעין שלוש",
+                      'הגפן': "מעין שלוש",
+                      'העץ': create_souls_pray,
+                      'האדמה': create_souls_pray}
+    pray_text = "סדר הברכות: ברכת המוציא קודמת לברכת מזונות, שקודמת לברכת הגפן , " \
+                "שקודמת לברכת העץ, שקודמת (לחלק מהשיטות) לברכת האדמה, שקודמת לברכת שהכל\n\n " \
+                "ברכה ראשונה: "
+
+    for pray in predictions:
+        pray_text += first_pray_dict[pray] + "\n\n"
+
+    pray_text += "ברכה אחרונה: "
+
+    for pray in predictions:
+        pray_text += last_pray_dict[pray]
+
+    return pray_text
 
 
 class MainScreen(FloatLayout):
@@ -50,15 +81,29 @@ class MainScreen(FloatLayout):
         xcamera.index = (xcamera.index + 1) % 2  # toggle between 0 and 1
 
     def predict(self, instance):
-        # TODO
-        # figure out how to get the images in the current directory using globbing, the below might help
-        # import glob
-        # source_path = "my_path"
-        # image_files = [source_path + '/' + f for f in glob.glob('*.png')]
-        # images_path = glob.glob('r\\d{4}-\\d{2}-\\d{2}*.jpg')
-        # first_image = next(iter(images_path), None)
+        # Get the current working directory
+        cwd = os.getcwd()
 
-        first_image = "food.jpg"
+        # Initialize an empty list to store the files
+        date_files = []
+
+        # Iterate through all the subdirectories and files in the current directory
+        for root, dirs, files in os.walk(cwd):
+            for file in files:
+                # Check if the file name matches the specified format
+                if file.endswith(".jpg") and file.split(".")[0].replace("_", " ").replace("-", " ").split():
+                    # Append the file to the list
+                    date_files.append(os.path.join(root, file))
+
+        # Sort the list of files by date
+        sorted_date_files = sorted(date_files, key=lambda x: os.path.getmtime(x), reverse=True)
+
+        # print(sorted_date_files)
+
+        # TODO
+        # if there is no file available - print an error, and ask to take a picture first
+        first_image = sorted_date_files[0]
+        # first_image = "food.jpg"
         # infer on a local image
         output = self.model.predict(first_image).json()
 
@@ -74,12 +119,28 @@ class MainScreen(FloatLayout):
         for key, value in new_dict.items():
             print(key, '    ', value)
 
-        # TODO
-        # 1. create a dictionary with the name of the pray as the key and the text as the value
-        # for example 'שהכל' : 'ברוך את ... שהכל נהיה בדברו'
-        # 2. create the whole text to pray using the dictionary and the keys of new_dict
-        # 3. create a GUI to display it (maybe use the background of the first presentation) and write above the text
+        pray_text = pray(new_dict.keys())
 
+        self.screen_manager.get_screen('pray_screen').pray_text = pray_text
+        self.screen_manager.current = 'pray_screen'
+
+        # create an instance of the PrayScreen
+        pray_screen = PrayScreen(name='pray_screen')
+
+        # create an instance of the ScreenManager
+        screen_manager = ScreenManager()
+
+        # add the PrayScreen and MainScreen to the ScreenManager
+        screen_manager.add_widget(pray_screen)
+        screen_manager.add_widget(MainScreen(screen_manager=screen_manager))
+        print(pray_text)
+
+        # TODO
+        # 1. create a dictionary with the name of the pray as the key and the text as the value check
+        # for example 'שהכל' : 'ברוך את ... שהכל נהיה בדברו'
+        # 2. create the whole text to pray using the dictionary and the keys of new_dict check
+        # 3. create a GUI to display it (maybe use the background of the first presentation) and write above the text
+        # 4. create a button to go back to the camera screen
 
 if __name__ == '__main__':
-    runTouchApp(MainScreen())
+    runTouchApp(MainScreen)
